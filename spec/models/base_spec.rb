f3796ddd0100487fe9f08df6a85dfeef44c31ebf
Shinji Kobayashi <skoba@moss.gr.jp>
2018-02-07 20:55:12 +0900

    making configuration file and session management basis

--- spec/models/base_spec.rb
+++ spec/models/base_spec.rb
@@ -5,5 +5,20 @@ RSpec.describe Base, type: :model do
     it 'site_url is defined in configuration'  do
       expect(Base.site_url).to eq Ethercis['site']
     end
+
+    example 'port' do
+      expect(Base.port).to eq Ethercis['port']
+    end
+
+    example 'path_prefix' do
+      expect(Base.path_prefex).to eq Ethercis['path_prefix']
+    end
+  end
+
+  describe 'session management' do
+    
+    example 'set ehr_session' do
+      expect{ Base.send(:set_ehr_session) }.not_to raise_error
+    end
   end
 end
